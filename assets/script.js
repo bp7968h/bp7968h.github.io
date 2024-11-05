be517c132b589f636c673dbc60730c95a4105377
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-11-05 20:42:09 +0000

    chore: clean old files

--- assets/script.js
+++
@@ -1,9 +0,0 @@
-// Show "To Top" button only when scrolling down
-window.addEventListener('scroll', function() {
-  const toTopButton = document.getElementById('toTop');
-  if (window.scrollY > 100) {
-    toTopButton.style.display = 'block';
-  } else {
-    toTopButton.style.display = 'none';
-  }
-});
