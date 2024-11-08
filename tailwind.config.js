a330842173c9c6071f196e5b54fbcdaf05508648
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-11-08 23:37:50 +0000

    feat: add journey section

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,15 @@ module.exports = {
         '17': '68px',
         '26': '104px'
       },
+      colors: {
+        'dark-gray-1': '#3f3f46',
+        'dark-gray-2': '#52525b',
+        'dark-gray-3': '#71717a',
+        'dark-gray-4': '#27272a',
+        'dark-gray-5': '#18181b',
+        'light-gray-1': '#a1a1aa',
+        'light-gray-2': '#d4d4d8',
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
